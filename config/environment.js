7849f838c8bb40194965185f6d56d486497884ef
Davide Schiera <davide@sysdig.com>
2015-10-12 16:58:29 -0700

    Fixed content security policy.

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,12 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src':  "'self' 'unsafe-inline'",
+      'style-src':   "'self' 'unsafe-inline'"
     }
   };
 
