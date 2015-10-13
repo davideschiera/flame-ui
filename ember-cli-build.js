8d3415acf734f533427b7ac71a78af913820ad5d
Davide Schiera <davide@sysdig.com>
2015-10-12 17:23:26 -0700

    Inline resources in production.
    (partially reverted from commit f1e6a174eb9e7163ca94bf7bb6a1489c9b24ee60)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,36 +11,6 @@ module.exports = function(defaults) {
         }
     });
 
-    if (app.env === 'production') {
-        //
-        // Inline Javascript and CSS files inline
-        //
-        app.options.inlineContent = {
-            'app-js':       'dist/assets/flame-ui.js',
-            'app-css':      'dist/assets/flame-ui.css',
-            'vendor-js':    'dist/assets/vendor.js',
-            'vendor-css':   'dist/assets/vendor.css'
-        };
-    } else {
-        //
-        // Leave Javascript and CSS outside the HTML for easier debugging
-        //
-        app.options.inlineContent = {
-            'app-js': {
-                content: '<script src="assets/flame-ui.js"></script>'
-            },
-            'app-css': {
-                content: '<link rel="stylesheet" href="assets/flame-ui.css">'
-            },
-            'vendor-js': {
-                content: '<script src="assets/vendor.js"></script>'
-            },
-            'vendor-css': {
-                content: '<link rel="stylesheet" href="assets/vendor.css">'
-            }
-        };
-    }
-
     app.import(app.bowerDirectory + '/d3/d3.v2.js');
 
     return app.toTree();
