f6cb072debf09df7d353e41ba9dab5f63980a33c
Davide Schiera <davide@sysdig.com>
2015-10-14 01:17:35 -0700

    Refactoring.

--- app/components/flame-ui.js
+++ app/components/flame-ui.js
@@ -79,7 +79,7 @@ export default Ember.Component.extend({
                             {
                                 coloring:       svColorMode,
                                 growDown:       svGrowDown,
-                                getNodeColor:    me.getNodeColor.bind(me)
+                                getNodeColor:   me.getNodeColor.bind(me)
                             });
                         svPopoutBox.style('z-index', 1);
                         svPopoutBox.style('opacity', 1);
@@ -465,73 +465,3 @@ FlameGraph.prototype.zoomSet = function (cd) {
     this.fg_text.transition().duration(svTransitionTime).
         attr('x', this.fg_x);
 };
-
-// /*
-//  * This function is copied directly from lib/color.js.  It would be better if we
-//  * could share code between Node.js and web JS.
-//  */
-// function convertHsvToRgb(h, s, v)
-// {
-//     var r, g, b;
-//     var i;
-//     var f, p, q, t;
-
-//     if (s === 0) {
-//         /*
-//          * A saturation of 0.0 is achromatic (grey).
-//          */
-//         r = g = b = v;
-
-//         return ([ Math.round(r * 255), Math.round(g * 255),
-//             Math.round(b * 255) ]);
-//     }
-
-//     h /= 60; // sector 0 to 5
-
-//     i = Math.floor(h);
-//     f = h - i; // fractional part of h
-//     p = v * (1 - s);
-//     q = v * (1 - s * f);
-//     t = v * (1 - s * (1 - f));
-
-//     switch (i) {
-//         case 0:
-//             r = v;
-//             g = t;
-//             b = p;
-//             break;
-
-//         case 1:
-//             r = q;
-//             g = v;
-//             b = p;
-//             break;
-
-//         case 2:
-//             r = p;
-//             g = v;
-//             b = t;
-//             break;
-
-//         case 3:
-//             r = p;
-//             g = q;
-//             b = v;
-//             break;
-
-//         case 4:
-//             r = t;
-//             g = p;
-//             b = v;
-//             break;
-
-//         default: // case 5:
-//             r = v;
-//             g = p;
-//             b = q;
-//             break;
-//     }
-
-//     return ([ Math.round(r * 255),
-//         Math.round(g * 255), Math.round(b * 255) ]);
-// }
