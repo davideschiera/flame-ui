c051cdee2917c577f31fc2dd9acd10cbb72d67d6
Davide Schiera <davide@sysdig.com>
2015-10-14 02:51:13 -0700

    Patch d3.entries to exclude Ember prototypes objects.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+/* global d3 */
+
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
@@ -15,4 +17,18 @@ App = Ember.Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 
+//
+// Patch d3.entries to include only own property and avoid extra properties added by Ember prototypes.
+//
+d3.entries = function(map) {
+  var entries = [];
+  for (var key in map) {
+    if (map.hasOwnProperty(key)) {
+      entries.push({key: key, value: map[key]});
+    }
+  }
+  return entries;
+};
+
 export default App;
+
