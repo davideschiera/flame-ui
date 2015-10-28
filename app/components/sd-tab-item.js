2878aed3f24003b1869f2d25b71b5214953874af
FezVrasta <fez@tekhy.net>
2015-10-28 17:14:58 +0100

    improved style of tabs component

--- app/components/sd-tab-item.js
+++ app/components/sd-tab-item.js
@@ -5,5 +5,28 @@ export default Ember.Component.extend({
     classNames:        [ 'sd-tab-item' ],
     classNameBindings: [ 'isActive:-active' ],
 
-    isActive: false
+    // Register this tab as part of the parent tabs-list, doing so we'll know how many tabs have our list
+    // and we can set the first tab as the default activated
+    setup: Ember.on('didInsertElement', function() {
+       this.send('registerTab');
+    }),
+
+    // Use this property to know if the current tab is the active one
+    isActive: Ember.computed('activeTab', function() {
+        return this.get('activeTab') === this.get('elementId');
+    }),
+
+    // Clicking on this tab will activate it
+    click() {
+        this.send('activateTab');
+    },
+
+    actions: {
+        registerTab() {
+             this.sendAction('registerTab', this.get('elementId'));
+        },
+        activateTab() {
+            this.sendAction('activateTab', this.get('elementId'));
+        }
+    }
 });
--- app/components/sd-tabs-list.js
+++ app/components/sd-tabs-list.js
@@ -4,5 +4,21 @@ export default Ember.Component.extend({
     tagName:    'ul',
     classNames: [ 'sd-tabs-list' ],
 
-    activeTab:  null
+    // This is the list of tabs' IDs contained in this tabs-list
+    tabs: Ember.A([]),
+
+    // By default, set as active tab the first one
+    // this prop will then be overrided when a different one is activated
+    activeTab:  Ember.computed('tabs.[]', function() {
+        return this.get('tabs.0');
+    }),
+
+    actions: {
+        registerTab(tab) {
+            this.get('tabs').pushObject(tab);
+        },
+        activateTab(tab) {
+            this.set('activeTab', tab);
+        }
+    }
 });
